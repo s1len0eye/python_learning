a3e107e8922e8ae1df8c897c8d7b3ab6493d833c
s1len0eye <s1len0eye@gmail.com>
2018-03-13 23:11:31 +0800

    Able to use 'q' to exit

---
+++ alien_invasion/game_functions.py
@@ -0,0 +1,60 @@
+import sys
+import pygame
+
+from bullet import Bullet
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """如果还没有达到限制，就发射一颗子弹"""
+    # 创建新子弹，并将其加入到编组bullets中
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
+        
+        
+def check_keyup_events(event, ship):
+    """响应松开"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+def check_events(ai_settings, screen, ship, bullets):
+    """响应按键和鼠标事件"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+            
+def update_screen(ai_settings, screen, ship, bullets):
+    """更新屏幕上的图像，并切换到新屏幕"""
+    # 每次循环时都重绘屏幕
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    # 在飞船和外星人后面重绘所有子弹
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
+    
+def update_bullets(bullets):
+    """更新子弹的位置，并删除已消失子弹"""
+    # 更新子弹位置
+    bullets.update()
+    # 删除已消失子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
